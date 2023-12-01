bc0892d6acec96c0782e02fc23bd6a4e06cb4834
onkar <jadhavonkar333@gmail.com>
2023-12-01 15:42:57 +0530

    assignment25

---
+++ MySharedLibrary/MainApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "123";
+
+            // Example usage of utility methods
+            bool isNumber = MySharedLibrary.Class1.IsNumber(userInput);
+            MySharedLibrary.Class1.LogMessage($"User input: {userInput}, IsNumber: {isNumber}");
+            string encryptedText = MySharedLibrary.Class1.Encrypt("SensitiveData");
+            Console.WriteLine(encryptedText);
+        }
+    }
+}
---
+++ MySharedLibrary/MySharedLibrary/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySharedLibrary
+{
+    public class Class1
+    {
+        public static bool IsNumber(string input)
+        {
+            return int.TryParse(input, out _);
+        }
+
+        public static void LogMessage(string message)
+        {
+            Console.WriteLine($"[LOG] {DateTime.Now}: {message}");
+        }
+
+        public static string Encrypt(string input)
+        {
+            // Implement encryption logic here
+            return $"Encrypted: {input}";
+        }
+    }
+}
